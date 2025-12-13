bae1da46866d580a65fda61c38e270c7b46deab7
Mo <muhamadyesufu2000@gmail.com>
2025-12-13 14:42:48 +0000

    init

--- backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-# backend/users/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Exemple : endpoint pour lister les utilisateurs
-    path("", views.UserListView.as_view(), name="user-list"),
-]
