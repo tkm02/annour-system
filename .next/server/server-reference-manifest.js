cc931701163a29c52aff9716c348a97c952cfd77
MAMADOU TUO <mamadoutuo77@gmail.com>
2026-01-02 03:34:30 +0000

    commit manage

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KL4ccq8wdJRmOZNXMbfrxHf4k0PMBjav8Z46dMbacHA=\"\n}"
+=======
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cq8XB/zNdbttOpz5i3Us1u8VXavWaw73MoUHIoENZps=\"\n}"
+>>>>>>> 6ecc229 (add feedback2)
